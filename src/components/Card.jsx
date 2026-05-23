c40abe0e09ba9cc2bd38b478cb2388b78dbd7d2d
jose-g315 <120354063+jose-g315@users.noreply.github.com>
2026-05-23 12:45:21 -0500

    add function to randomize card values which later will be an ID

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-export default function Card({ text, value, handleCardClick }) {
+export default function Card({ value, handleCardClick }) {
   function handleClick() {
     handleCardClick(value);
   }
   return (
     <button type='button' onClick={handleClick}>
-      {text}
+      {value}
     </button>
   );
 }
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -3,16 +3,30 @@ import { useState } from 'react';
 
 export default function CardContainer({ increaseScore, setScore }) {
   const [clickedCards, setClickedCards] = useState([]);
-
-  const cards = [
+  const [cards, setCards] = useState([
     { text: 'One', value: 1 },
     { text: 'Two', value: 2 },
     { text: 'Three', value: 3 },
     { text: 'Four', value: 4 },
     { text: 'Five', value: 5 },
     { text: 'Six', value: 6 },
-  ];
+  ]);
 
+  function randomizeCards() {
+    const randomArray = [];
+    for (let i = 0; randomArray.length < 6; i++) {
+      let randomNumber = Math.floor(Math.random() * 6) + 1;
+      if (!randomArray.includes(randomNumber)) {
+        randomArray.push(randomNumber);
+      }
+    }
+    const newCards = cards.map((card, index) => ({
+      ...card,
+      value: randomArray[index], // assign random position
+    }));
+
+    setCards(newCards);
+  }
   function handleCardClick(value) {
     // Add the clicked card's value to the clickedCards array
     setClickedCards((prev) => [...prev, value]);
@@ -25,18 +39,14 @@ export default function CardContainer({ increaseScore, setScore }) {
       setClickedCards([]);
     } else {
       increaseScore();
+      randomizeCards();
     }
   }
 
   return (
     <div className='card-container'>
       {cards.map((entry) => (
-        <Card
-          key={entry.value}
-          text={entry.text}
-          value={entry.value}
-          handleCardClick={handleCardClick}
-        />
+        <Card key={entry.value} value={entry.value} handleCardClick={handleCardClick} />
       ))}
     </div>
   );
